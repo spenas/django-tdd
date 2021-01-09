f2c140980917a54d587ee66776b89f683df6aadb
Santiago Pena <spenas@unal.edu.co>
2021-01-09 11:11:51 -0500

    users made

--- app/app/settings.py
+++ app/app/settings.py
@@ -30,14 +30,17 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [ 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
